4d63ea2a5d1856f01e71eae14f3bb45d52c098bf
wangxwlt <wangxwlt@gmail.com>
2017-06-07 16:42:03 +0800

    Update cifar10.py

--- tutorials/image/cifar10/cifar10.py
+++ tutorials/image/cifar10/cifar10.py
@@ -135,7 +135,7 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
       tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
   if wd is not None:
     weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
-    tf.add_to_collection('losses', weight_decay)
+    tf.add_to_collection('losses', weight_decay) #save the var once, use it everwhere
   return var
 
 
